b9aae46c45416cced34f13405b050c8c80bb66ba
teklumezgebo <mezge002@umn.edu>
2024-01-12 17:57:46 -0600

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -7,16 +7,16 @@ def save(session, dog):
     pass
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == f"{name}").first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == f"{name}", Dog.breed == f"{breed}").first()
 
 def update_breed(session, dog, breed):
-    pass
+    session.query(Dog).filter(Dog.id == dog.id).update({dog.breed: f"{breed}"})
